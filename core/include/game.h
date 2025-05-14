bcab9bc5b82dd6fa4bd6678182b36d6c5fabbfaf
benjamin layson <benjaminlayson0@gmail.com>
2025-05-14 16:37:10 +0200

    first push

---
+++ core/include/game.h
@@ -0,0 +1,26 @@
+﻿//
+// Created by benja on 14/05/2025.
+//
+
+
+#ifndef GAME_H
+#define GAME_H
+#include <SFML/Graphics.hpp>
+
+
+class Game
+{
+public:
+
+    Game();
+    void run();
+
+private:
+    sf::RenderWindow _window;
+    sf::Texture grass_texture_;
+
+};
+
+
+
+#endif //GAME_H
---
+++ main.cc
@@ -0,0 +1,12 @@
+﻿
+
+#include "game.h"
+
+
+int main()
+{
+    Game game;
+    game.run();
+
+    return 0;
+}
